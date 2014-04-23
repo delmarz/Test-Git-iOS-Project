21e12d3d899e7e976c5ab9b0c7bf6f5d3b3991ff
delmarz <delmarz@delmarzs-Mac-Pro.local>
2014-04-23 15:47:01 +0800

    added a test class

---
+++ Git Test/DMZTest.h
@@ -0,0 +1,13 @@
+//
+//  DMZTest.h
+//  Git Test
+//
+//  Created by delmarz on 4/23/14.
+//  Copyright (c) 2014 Dream Code. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMZTest : NSObject
+
+@end
---
+++ Git Test/DMZTest.m
@@ -0,0 +1,13 @@
+//
+//  DMZTest.m
+//  Git Test
+//
+//  Created by delmarz on 4/23/14.
+//  Copyright (c) 2014 Dream Code. All rights reserved.
+//
+
+#import "DMZTest.h"
+
+@implementation DMZTest
+
+@end
